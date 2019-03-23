cf02d5660dcc628afe1bb72551669659c675b239
alexzota <zota.andreialexandru@gmail.com>
2019-03-23 14:28:40 +0200

    removed a folder

--- Site-Imperiul-Bizantin-master/jsdoc.js
+++
@@ -1,9 +0,0 @@
-var myWindow;
-
-function openWin() {
-  myWindow = window.open("https://ro.wikipedia.org/wiki/Imperiul_Roman_de_R%C4%83s%C4%83rit","",  "width=200,height=100");
-}
-
-function closeWin() {
-  myWindow.close();
-}
